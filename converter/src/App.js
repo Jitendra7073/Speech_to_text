f2b9dee754e52025fc1db4c58df9943d7f0fde3a
jeetsuthar <jeetsuthar151@gmail.com>
2024-05-25 15:13:04 +0530

    Speech to Text converter

---
+++ converter/src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+
+const App = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+  const [isCopied, setCopied] = useClipboard(
+    transcript ? transcript : "Please say something",
+    {
+      successDuration: 5000,
+    }
+  );
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h3>Speech to Text Converter <span style={{ color: listening ? "green" : "red", fontSize:"10px", marginLeft:"5px" }}>
+          {listening ? "ON" : "OFF"}
+        </span></h3>
+      </div>
+      
+      <div className="tagline">
+        <p>" Speak Freely, Transcribe Instantly. "</p>
+      </div>
+
+      <div className="listening-input">
+      <p>{transcript? transcript : "Please say something"} </p>  
+      </div>
+
+      <div className="buttons">
+        <button onClick={SpeechRecognition.startListening} style={{color : listening ? "green":"black"}}
+        className="start">Start</button>
+        <button onClick={SpeechRecognition.stopListening} className="stop">Stop</button>
+        <button onClick={resetTranscript}   className="reset">Reset</button>
+        <button onClick={setCopied} className="copy">Copy{isCopied ? "👍" : ""}</button>
+      </div>
+
+      
+     <footer>
+     <p className="copyright">Copyright © 2024 All rights reserved by jeetsuthar</p>
+     </footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ converter/src/components/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const index = () => {
+  return (
+    <div>
+      <p className="copyright">Copyright © 2024 All rights reserved.</p>
+    </div>
+  )
+}
+
+export default index
